5de948a62c9983744dda813a131a9dbb09943e96
tech-ml-quant-luv <luvratan.techengineer@gmail.com>
2026-05-05 23:25:58 +0530

    exit price is now LTP

--- order_execution/exit_positions.py
+++ order_execution/exit_positions.py
@@ -20,10 +20,10 @@ from authentication import get_access_token
 def create_exit_position(orderType,ltp):
     if orderType == 1:
         orderType = "short"
-        limitPrice = ltp*(1-0.001)
+        limitPrice = ltp
     else:
         orderType="long"
-        limitPrice = ltp*(1+0.001)
+        limitPrice = ltp
 
     return orderType, limitPrice
 
